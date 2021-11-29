7667c2093b09359310fca16382627804678d0c13
JuYeon <JuYeon@LAPTOP-2UIR20D1>
2021-11-29 23:27:11 +0900

    2021-11-29/23:26/김주연/spaceList.jsp, spaceInsert.jsp 추가

--- src/main/java/kr/or/space/model/dao/SpaceDao.java
+++ src/main/java/kr/or/space/model/dao/SpaceDao.java
@@ -1,11 +1,25 @@
 package kr.or.space.model.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import kr.or.space.model.vo.Space;
 
 @Repository
 public class SpaceDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public int insertSpace(Space s) {
+		return sqlSession.insert("space.insertSpace",s);
+	}
+
+	public ArrayList<Space> selectAllSpace() {
+		List<Space> list = sqlSession.selectList("space.selectAllSpace");
+		return (ArrayList<Space>)list;
+	}
 }
