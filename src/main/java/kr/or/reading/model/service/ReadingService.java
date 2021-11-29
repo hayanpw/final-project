1d14010e76752447084ff0c87d011dc8c33228b1
Canary <Canary@218.232.0.196>
2021-11-29 15:56:42 +0900

    2021-11-29/15:54/오준택 mybatis-config reading/black 추가 readingBlack vo 추가 /readingList.do 추가(주석참조) readingList.jsp 추가 readingNotice.jsp 수정 readingSQL.xml 추가 servlet-context.xml 수정

--- src/main/java/kr/or/reading/model/service/ReadingService.java
+++ src/main/java/kr/or/reading/model/service/ReadingService.java
@@ -1,5 +1,22 @@
 package kr.or.reading.model.service;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.reading.model.dao.ReadingDao;
+import kr.or.reading.model.vo.ReadingBlack;
+
+@Service
 public class ReadingService {
 
+	@Autowired
+	private ReadingDao dao;
+
+
+	public ReadingBlack selectOneBlackList(String memberId) {
+		ReadingBlack rb = dao.selectOneBlackList(memberId);
+		return rb;
+	}
 }
