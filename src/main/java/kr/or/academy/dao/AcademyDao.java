ab8ea720b248ea4ffa7ea26323be3091fbe2fa3d
USER <USER@DESKTOP-8UDI8FI>
2021-11-29 17:47:30 +0900

    2021-11-29 17:46 임한솔 servlet-context 에  academy scan범위 추가 dao service 세팅완료

--- src/main/java/kr/or/academy/dao/AcademyDao.java
+++ src/main/java/kr/or/academy/dao/AcademyDao.java
@@ -1,5 +1,12 @@
 package kr.or.academy.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AcademyDao {
 
+	@Autowired
+	private SqlSession sqlSession;
 }
--- src/main/java/kr/or/academy/service/AcademyService.java
+++ src/main/java/kr/or/academy/service/AcademyService.java
@@ -1,5 +1,13 @@
 package kr.or.academy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.academy.dao.AcademyDao;
+
+@Service
 public class AcademyService {
 
+	@Autowired
+	private AcademyDao dao;
 }
