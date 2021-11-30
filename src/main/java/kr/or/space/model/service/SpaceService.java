7d532b8f9fdbe33a41d50df3e899e2812af562d7
06eeYeon <94663239+06eeYeon@users.noreply.github.com>
2021-11-30 16:51:14 +0900

    2021-11-30/16:50/김주연/공간등록기능 구현, 공간 vo 생성

--- src/main/java/kr/or/space/model/service/SpaceService.java
+++ src/main/java/kr/or/space/model/service/SpaceService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.space.model.dao.SpaceDao;
+import kr.or.space.model.vo.FileVO;
 import kr.or.space.model.vo.Space;
 
 @Service
@@ -13,8 +14,18 @@ public class SpaceService {
 	@Autowired
 	private SpaceDao dao;
 
-	public int insertSpace(Space s) {
-		return dao.insertSpace(s);
+	//공간등록 + 파일 업로드
+	public int insertSpace(Space s, ArrayList<FileVO> list) {
+		int result1 = dao.insertSpace(s);
+		int result=0;
+		if(result>0) {
+			int spaceNo = dao.selectSpaceNo();
+			for(FileVO fv : list) {
+				fv.setSpaceNo(spaceNo);
+				result += dao.insertFile(fv);
+			}
+		}
+		return result;		
 	}
 
 	public ArrayList<Space> selectAllSpace() {
