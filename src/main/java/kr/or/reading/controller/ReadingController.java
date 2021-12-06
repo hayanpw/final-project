c1c7df128ceec4b07c03f1bcb9e9d64308539e5d
Canary <Canary@175.113.159.34>
2021-12-06 14:11:26 +0900

    2021-12-06/14:07/오준택  - 좌석 선택 이전단계 추가  - 좌석 선택 1개만 할 수 있게 추가  - 예약 및 선택 좌석 DB에 저장 구현 완료  - 아이디 하나당 해당일 좌석 1개 예약 구현 완료  - 이미 선택된 좌석입니다 구현완료  - 예약완료 창 구현중

--- src/main/java/kr/or/reading/controller/ReadingController.java
+++ src/main/java/kr/or/reading/controller/ReadingController.java
@@ -24,11 +24,11 @@ public class ReadingController {
 		//로그인 해야 열람실 안내를 들어올수있게
 		//로그인 구현되면 return 위치 바꿔야됌
 		if(session.getAttribute("m")==null) {
-			return "reading/readingNotice";
-		}else {
 			model.addAttribute("msg","로그인이 필요한 서비스입니다.");
-			model.addAttribute("loc", "/");
+			model.addAttribute("loc", "/loginFrm.do");
 			return "common/msg";		
+		}else {
+			return "reading/readingNotice";
 		}
 	}
 	
@@ -50,7 +50,43 @@ public class ReadingController {
 	
 	@RequestMapping(value="/readingSeat.do")
 	public String readingSeat(Reading re, Model model) {
+		//날짜 넘겨줌
 		model.addAttribute("re", re);
 		return "reading/readingSeat";
+	}
+	
+	@RequestMapping(value="/readingInsert.do")
+	public String readingInsert(Reading re, Model model) {
+		//선 좌석조회 후 insert
+		//같은날 같은 좌석 선택시 이선좌출력
+		//선택사항은 update로 selectOneId
+		Reading re1 = service.selectOneNum(re);
+		Reading re2 = service.selectOneId(re);
+		if(re1!=null) {
+			model.addAttribute("msg","이미 선택된 좌석입니다.");
+			model.addAttribute("loc", "/readingList.do");
+		}else if(re2!=null){
+			//좌석 선택으로 넘어갈때로 옮길예정
+			//이 문구 뜨고 예매내역으로 이동예정
+			model.addAttribute("msg",re.getReadingDay()+"일은 이미 예약하셨습니다.");
+			model.addAttribute("loc", "/readingList.do");
+		}else {
+			int result = service.insertReading(re);
+			if(result>0){
+				model.addAttribute("re", re);
+				model.addAttribute("msg",re.getReadingDay()+"일 "+re.getReadingNum()+"번 좌석을 예약하셨습니다.");
+				model.addAttribute("loc", "/readingOption.do");
+			}else {
+				model.addAttribute("msg","예약 오류. 다시 시도해주세요.");
+				model.addAttribute("loc", "/readingList.do");
+			}
+		}
+		return "common/msg";
+	}
+	
+	@RequestMapping(value="/readingOption.do")
+	public String readingOption(Reading re, Model model) {
+		model.addAttribute("re", re);
+		return "reading/readingOption";
 	}
 }
