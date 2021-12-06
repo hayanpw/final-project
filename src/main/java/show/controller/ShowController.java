8166582652a70ceb5aecc429518502ad5ba52ae4
user1 <user1@DESKTOP-RQ9DGL7>
2021-12-06 16:45:30 +0900

    2021.12.06/16:43/최승준 - 부가정보 -> 예매및취소로 수정 - 예매및취소 내용 추가

--- src/main/java/show/controller/ShowController.java
+++ src/main/java/show/controller/ShowController.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
